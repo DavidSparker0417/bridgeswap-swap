95dd0fc22bdcdc717657b4e72759314528684b0e
OkekeChristian24 <christian.okeke24@gmail.com>
2022-03-31 09:52:57 -0700

    fix: implement deployable

---
+++ vital-folders/@pancakeswap-libs/uikit/dist/components/Card/types.d.ts
@@ -0,0 +1,28 @@
+import { HTMLAttributes } from "react";
+import { SpaceProps } from "styled-system";
+import { Colors } from "../../theme/types";
+export interface CardRibbonProps {
+    variantColor?: keyof Colors;
+    text: string;
+    ribbonPosition?: "right" | "left";
+}
+export declare type CardTheme = {
+    background: string;
+    boxShadow: string;
+    boxShadowActive: string;
+    boxShadowSuccess: string;
+    boxShadowWarning: string;
+    cardHeaderBackground: {
+        default: string;
+        blue: string;
+        violet: string;
+    };
+    dropShadow: string;
+};
+export interface CardProps extends SpaceProps, HTMLAttributes<HTMLDivElement> {
+    isActive?: boolean;
+    isSuccess?: boolean;
+    isWarning?: boolean;
+    isDisabled?: boolean;
+    ribbon?: React.ReactNode;
+}
---
+++ vital-folders/@pancakeswap-libs/uikit/dist/components/TabMenu/Tab.d.ts
@@ -0,0 +1,4 @@
+import React from "react";
+import { TabProps } from "./types";
+declare const Tab: React.FC<TabProps>;
+export default Tab;
